b98594dc41b8f252d39eed368e2b30df87dc528f
Enes <enesfplt@gmail.com>
2024-07-15 11:20:11 -0400

    Quote Generator

--- script.js
+++ script.js
@@ -1,76 +1,100 @@
-$(document).ready(function() {
-    var apikey = "5fhC6DvyyBisQH3xI0qYuBSJF6x0oJ32Pg5NUkwH7n8zaQ286lBXdgaD";
-    var imagesData = [];
-    var quotesData = [];
+$(document).ready(function(){
 
-    // Disable the new quote button initially
-    $("#newquote").prop("disabled", true);
+    var apikey ="5fhC6DvyyBisQH3xI0qYuBSJF6x0oJ32Pg5NUkwH7n8zaQ286lBXdgaD"
+    var backgroundcount=0;
+    var authorcount=0;
+    let imagesData=[];
+    let quotesData=[];
 
-    // Fetch data and enable button when ready
-    $.when(quotesearch(), imagesearch()).done(function() {
-        $("#newquote").prop("disabled", false);
-    });
+    navigator.clipboard.writeText("The only true wisdom is in knowing you know nothing. - Socrates");
 
-    $("#newquote").click(function(event) {
+    quotesearch();
+    imagesearch();
+    
+
+    $("#newquote").click(function (event){
         event.preventDefault();
-        if (quotesData.length > 0 && imagesData.length > 0) {
-            const quoterandomizer = Math.floor(Math.random() * quotesData.length);
-            const backgroundrandomizer = Math.floor(Math.random() * imagesData.length);
+        const quoterandomizer = Math.floor(Math.random() * quotesData.length)
+        const backgroundrandomizer =  Math.floor(Math.random() * imagesData.length)
 
-            $('.textbox').animate({ opacity: 0 }, 500, function() {
-                $(this).animate({ opacity: 1 }, 500);
-                $('#text').html(quotesData[quoterandomizer].quote);
-            });
+        $('.textbox').animate({ opacity: 0 }, 500, function () {
+            $(this).animate({ opacity: 1 }, 500);
+            $('#text').html(quotesData[quoterandomizer].quote);
+          });
+        
+        $('.author').animate({ opacity: 0 }, 500, function () {
+            $(this).animate({ opacity: 1 }, 500);
+            $('#author').html("- " + quotesData[quoterandomizer].source);
+          });
 
-            $('.author').animate({ opacity: 0 }, 500, function() {
-                $(this).animate({ opacity: 1 }, 500);
-                $('#author').html("- " + quotesData[quoterandomizer].source);
-            });
+        $('#tweet-quote').attr(
+            'href',
+            'https://twitter.com/intent/tweet?hashtags=quotes&text=' +
+            encodeURIComponent('"' + quotesData[quoterandomizer].quote + '" ' + quotesData[quoterandomizer].source)
+        );
 
-            $('#tweet-quote').attr(
-                'href',
-                'https://twitter.com/intent/tweet?hashtags=quotes&text=' +
-                encodeURIComponent('"' + quotesData[quoterandomizer].quote + '" ' + quotesData[quoterandomizer].source)
-            );
+        $("#copy-quote").click(function (event) {
+            event.preventDefault();
+            console.log("copy")
 
-            $("#copy-quote").off("click").on("click", function(event) {
-                event.preventDefault();
-                navigator.clipboard.writeText(quotesData[quoterandomizer].quote + " - " + quotesData[quoterandomizer].source);
-            });
+            navigator.clipboard.writeText(quotesData[quoterandomizer].quote + " - " + quotesData[quoterandomizer].source);
+        
+        });
 
-            $("body").css("background-image", "url(" + imagesData[backgroundrandomizer].src.original + ")");
-            $(":root").get(0).style.setProperty("--coloruse", imagesData[backgroundrandomizer].avg_color);
-        } else {
-            console.error("Data not loaded properly.");
-        }
+        
+        $("body").css("background-image", "url(" + imagesData[backgroundrandomizer].src.original + ")");
+        $(":root").get(0).style.setProperty("--coloruse", imagesData[backgroundrandomizer].avg_color)
+
+        
+
+        //sol 1: create div, z index -1, put image inside it, new click replaces img
+        //get 20 links of images
+
+        
     });
 
-    function imagesearch() {
+    function imagesearch(){
         return $.ajax({
             method: 'GET',
             beforeSend: function(xhr) {
                 xhr.setRequestHeader("Authorization", apikey);
             },
+
             url: "https://api.pexels.com/v1/search?query=nature&per_page=80",
+
             success: function(data) {
+            
+
                 imagesData = data.photos;
+                
+                
             },
-            error: function(error) {
-                console.log(error);
+
+            error: function (error){
+                console.log(error)
             }
-        });
+        })
     }
 
-    function quotesearch() {
+    function quotesearch(){
         return $.ajax({
             method: 'GET',
+
             url: "https://philosophy-quotes-api.glitch.me/quotes",
+
             success: function(data) {
-                quotesData = data;
+                quotesData= data;
+                
+                
+                
             },
-            error: function(error) {
-                console.log(error);
+
+            error: function (error){
+                console.log(error)
             }
-        });
+        })
     }
+
+    
+
 });
