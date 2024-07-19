35f7db1c1f6649655411f4b145d447365d5125b7
Enes <enesfplt@gmail.com>
2024-07-19 17:22:32 -0400

    Quote Generator

--- script.js
+++ script.js
@@ -1,100 +1,85 @@
-$(document).ready(function(){
+$(document).ready(function() {
+    // API key for accessing Pexels API
+    var apikey = "5fhC6DvyyBisQH3xI0qYuBSJF6x0oJ32Pg5NUkwH7n8zaQ286lBXdgaD";
+    var backgroundcount = 0;
+    var authorcount = 0;
+    let imagesData = [];
+    let quotesData = [];
 
-    var apikey ="5fhC6DvyyBisQH3xI0qYuBSJF6x0oJ32Pg5NUkwH7n8zaQ286lBXdgaD"
-    var backgroundcount=0;
-    var authorcount=0;
-    let imagesData=[];
-    let quotesData=[];
-
+    // Copy initial quote to clipboard
     navigator.clipboard.writeText("The only true wisdom is in knowing you know nothing. - Socrates");
 
+    // Fetch quotes and images
     quotesearch();
     imagesearch();
-    
 
-    $("#newquote").click(function (event){
+    // Event handler for generating a new quote
+    $("#newquote").click(function(event) {
         event.preventDefault();
-        const quoterandomizer = Math.floor(Math.random() * quotesData.length)
-        const backgroundrandomizer =  Math.floor(Math.random() * imagesData.length)
 
-        $('.textbox').animate({ opacity: 0 }, 500, function () {
+        // Randomly select a quote and background image
+        const quoterandomizer = Math.floor(Math.random() * quotesData.length);
+        const backgroundrandomizer = Math.floor(Math.random() * imagesData.length);
+
+        // Animate the quote text change
+        $('.textbox').animate({ opacity: 0 }, 500, function() {
             $(this).animate({ opacity: 1 }, 500);
             $('#text').html(quotesData[quoterandomizer].quote);
-          });
-        
-        $('.author').animate({ opacity: 0 }, 500, function () {
+        });
+
+        // Animate the author text change
+        $('.author').animate({ opacity: 0 }, 500, function() {
             $(this).animate({ opacity: 1 }, 500);
             $('#author').html("- " + quotesData[quoterandomizer].source);
-          });
+        });
 
+        // Update tweet button with new quote
         $('#tweet-quote').attr(
             'href',
             'https://twitter.com/intent/tweet?hashtags=quotes&text=' +
             encodeURIComponent('"' + quotesData[quoterandomizer].quote + '" ' + quotesData[quoterandomizer].source)
         );
 
-        $("#copy-quote").click(function (event) {
+        // Event handler for copying the quote to clipboard
+        $("#copy-quote").click(function(event) {
             event.preventDefault();
-            console.log("copy")
-
+            console.log("copy");
             navigator.clipboard.writeText(quotesData[quoterandomizer].quote + " - " + quotesData[quoterandomizer].source);
-        
         });
 
-        
+        // Change background image and set new color property
         $("body").css("background-image", "url(" + imagesData[backgroundrandomizer].src.original + ")");
-        $(":root").get(0).style.setProperty("--coloruse", imagesData[backgroundrandomizer].avg_color)
-
-        
-
-        //sol 1: create div, z index -1, put image inside it, new click replaces img
-        //get 20 links of images
-
-        
+        $(":root").get(0).style.setProperty("--coloruse", imagesData[backgroundrandomizer].avg_color);
     });
 
-    function imagesearch(){
+    // Function to fetch images from Pexels API
+    function imagesearch() {
         return $.ajax({
             method: 'GET',
             beforeSend: function(xhr) {
                 xhr.setRequestHeader("Authorization", apikey);
             },
-
             url: "https://api.pexels.com/v1/search?query=nature&per_page=80",
-
             success: function(data) {
-            
-
                 imagesData = data.photos;
-                
-                
             },
-
-            error: function (error){
-                console.log(error)
+            error: function(error) {
+                console.log(error);
             }
-        })
+        });
     }
 
-    function quotesearch(){
+    // Function to fetch quotes from Philosophy Quotes API
+    function quotesearch() {
         return $.ajax({
             method: 'GET',
-
             url: "https://philosophy-quotes-api.glitch.me/quotes",
-
             success: function(data) {
-                quotesData= data;
-                
-                
-                
+                quotesData = data;
             },
-
-            error: function (error){
-                console.log(error)
+            error: function(error) {
+                console.log(error);
             }
-        })
+        });
     }
-
-    
-
 });
